3927285741786f8c1d2f558679b9b715bd9a4707
katareena <w10160177@gmail.com>
2020-05-03 16:22:11 +0300

    сборка

--- gulpfile.js
+++ gulpfile.js
@@ -1,107 +1,140 @@
 "use strict";
 
-var gulp = require("gulp");
-var plumber = require("gulp-plumber");
-var sourcemap = require("gulp-sourcemaps");
-var rename = require("gulp-rename");
-var server = require("browser-sync").create();
-var less = require("gulp-less");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var csso = require("gulp-csso");
-var imagemin = require("gulp-imagemin");
-var webp = require("gulp-webp");
-var svgstore = require("gulp-svgstore");
-var posthtml = require("gulp-posthtml");
-var include = require("posthtml-include");
-var del = require("del");
+const { task, src, dest, series, parallel, watch } = require("gulp");
+const server = require("browser-sync").create();
+const plumber = require("gulp-plumber");
+const rename = require("gulp-rename");
+const gulpIf = require("gulp-if");
+const newer = require("gulp-newer");
+const del = require("del");
+const sourcemap = require("gulp-sourcemaps");
+const less = require("gulp-less");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("gulp-cssnano");
+const uglify = require("gulp-uglify");
+const concat = require("gulp-concat");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const svgstore = require("gulp-svgstore");
+const posthtml = require("gulp-posthtml");
+const include = require("posthtml-include");
 
-gulp.task("css", function () {
-  return gulp.src("source/less/style.less")
-    .pipe(plumber())
-    .pipe(sourcemap.init())
-    .pipe(less())
-    .pipe(postcss([autoprefixer()]))
-    .pipe(csso())
-    .pipe(rename("style.min.css"))
-    .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("build/css"))
+const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+
+//-------------- очистка папки build --------------
+task("clean", () => {
+  return del(["build"]);
+});
+
+//-------------- собираем html ----------------------
+task("html", () => {
+  return src("source/*.html")
+    .pipe(dest("build"));
+})
+
+//-------------- копируем картинки ----------------------
+task("copy:img", () => {
+  return  src("source/img/**/*.{jpg,png,svg}")
+    .pipe(newer("build/img"))
+    .pipe(dest("build/img"))
     .pipe(server.stream());
 });
 
-gulp.task("server", function () {
-  server.init({
-    server: "build/",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
-  });
-
-  gulp.watch("source/less/**/*.less", gulp.series("css"));
-  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
-  gulp.watch("source/*.html", gulp.series("html", "refresh"));
+//-------------- копируем шрифты ----------------------
+task("copy:fonts", () => {
+  return  src("source/fonts/*.{woff,woff2}")
+    .pipe(newer("build/fonts"))
+    .pipe(dest("build/fonts"));
 });
 
-gulp.task("refresh", function(done) {
-  server.reload();
-  done();
+//-------------- создание webP изображений ----------
+task("webp", () => {
+  return src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(dest("build/img"));
 });
 
 //--------------оптимизация изображений------------
-gulp.task("images", function () {
-  return gulp.src("source/img/**/*.{png,jpg,svg}")
+task("img-min", () => {
+  return src("build/img/*.{png,jpg,svg}")
     .pipe(imagemin([
       imagemin.optipng({optimizationLevel: 3}),
       imagemin.mozjpeg({progressive: true}),
       imagemin.svgo()
     ]))
-    .pipe(gulp.dest("build/img"));
-})
-
-//--------------создание webP изображений----------
-gulp.task("webp", function () {
-  return gulp.src("source/img/**/*.{png,jpg}")
-    .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("build/img"));
-})
+    .pipe(dest("build/img"));
+});
 
 //--------------создание svg-спрайта---------------
-gulp.task("sprite", function () {
-  return gulp.src("source/img/icon-*.svg")
+task("sprite", () => {
+  return src("source/img/sprite-icons/*.svg")
     .pipe(svgstore({
       inlineSvg: true
     }))
     .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("source/img"));
+    .pipe(dest("build/img"));
 })
 
-//--------------gulp-posthtml----------------------
-gulp.task ("html", function () {
-  return gulp.src("source/*.html")
-    .pipe(posthtml([
-      include()
-    ]))
-    .pipe(gulp.dest("build"));
-})
-
-//--------------копирование из source в build------
-gulp.task("copy", function () {
-  return gulp.src([
-    "source/fonts/**/*.{woff,woff2}",
-    "source/img/**",
-    "source/js/**",
-    "source/*.ico"
-  ], {
-    base: "source"
-  })
-  .pipe(gulp.dest("build"));
+//-------------- собираем css ----------------------
+task("css", () => {
+  return src("source/less/style.less")
+    .pipe(plumber())
+    .pipe(gulpIf(isDev, sourcemap.init()))
+    .pipe(less())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(gulpIf(!isDev, cssnano()))
+    .pipe(rename("style.min.css"))
+    .pipe(gulpIf(isDev, sourcemap.write(".")))
+    .pipe(dest("build/css"))
+    .pipe(server.stream());
 });
 
-//--------------очистка папки build--------------
-gulp.task("clean", function () {
-  return del("build");
+//-------------- собираем js ----------------------
+task("js", () => {
+  return src([
+    "./node_modules/svg4everybody/dist/svg4everybody.js",
+    "./node_modules/picturefill/dist/picturefill.js",
+    "source/js/*.js"
+  ])
+    .pipe(gulpIf(isDev, sourcemap.init()))
+    .pipe(concat("bundle.min.js"))
+    .pipe(gulpIf(isDev, sourcemap.write(".")))
+    .pipe(gulpIf(!isDev, uglify()))
+    .pipe(dest("build/js"));
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
-gulp.task("start", gulp.series("build", "server"));
+task("server", () => {
+  server.init({
+    server: "build/",
+    reloadOnRestart: true,
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+});
+
+task("refresh", (done) => {
+  server.reload();
+  done();
+});
+
+task("watch", () => {
+  watch("source/less/**/*.less", series("css"));
+  watch("source/*.html", series("html", "refresh"));
+  watch("source/js/**/*.js", series("js"));
+  watch("source/img/**/*.{jpg,svg,png}", series("copy:img"));
+  watch("source/sprite-icons/*.svg", series("sprite", "refresh"));
+  watch("source/fonts/*.{woff,woff2}", series("copy:fonts"));
+});
+
+const buildTasks = ["clean", parallel(["html", "css", "js", "webp", "copy:fonts", "copy:img", "sprite"])];
+
+if (!isDev) {
+  buildTasks.push("img-min");
+}
+
+task("build", series(buildTasks));
+task("development", series("build", parallel("server", "watch")));
+task("default", series("development"));
